6b3d791dda9be75d3469692d30f27aa8da7aa6c9
RickaPrincy <rckprincy@gmail.com>
2023-12-31 11:09:43 +0300

    feat: handle pressed and released

--- src/read_devnode.cpp
+++ src/read_devnode.cpp
@@ -10,7 +10,7 @@
 void read_devnode(const char *devnode) {
     int fd = open(devnode, O_RDONLY);
     if (fd == -1) {
-        std::cerr << "Impossible d'ouvrir le périphérique clavier.\n";
+        std::cerr << "[ ERROR ]: Cannot open the keyboard.\n" << std::endl;
         return;
     }
 
@@ -18,14 +18,14 @@ void read_devnode(const char *devnode) {
 
     struct xkb_context *ctx = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
     if (!ctx) {
-        std::cerr << "Erreur lors de la création du contexte xkb.\n";
+        std::cerr << "[ ERROR ]: Cannot create context of xkb.\n" << std::endl;
         close(fd);
         return;
     }
 
     struct xkb_keymap *keymap = xkb_map_new_from_names(ctx, nullptr, XKB_KEYMAP_COMPILE_NO_FLAGS);
     if (!keymap) {
-        std::cerr << "Erreur lors de la création du keymap xkb.\n";
+        std::cerr << "[ ERRROR ]: Cannot create keymap xkb.\n" << std::endl;
         xkb_context_unref(ctx);
         close(fd);
         return;
@@ -33,7 +33,7 @@ void read_devnode(const char *devnode) {
 
     struct xkb_state *state = xkb_state_new(keymap);
     if (!state) {
-        std::cerr << "Erreur lors de la création de l'état xkb.\n";
+        std::cerr << "[ ERRROR ]: Cannot create state xkb.\n" << std::endl;
         xkb_keymap_unref(keymap);
         xkb_context_unref(ctx);
         close(fd);
@@ -48,11 +48,12 @@ void read_devnode(const char *devnode) {
                 xkb_keysym_t sym = xkb_state_key_get_one_sym(state, keycode);
 
                 xkb_keysym_t unicode = xkb_keysym_to_utf32(sym);
-
+                
+                std::string status = ev.value == 0 ? "released" : "pressed";
                 if (unicode != XKB_KEY_NoSymbol) {
-                    std::cout << "Touche : " << (char)unicode << std::endl;
+                    std::cout << "Key : "  << (char)unicode << " was " << status  << std::endl;
                 } else {
-                    std::cout << "Touche non gérée (code : " << ev.code << ")" << std::endl;
+                    std::cout << "Key not supported (code : " << ev.code << ")" << std::endl;
                 }
             }
         }
